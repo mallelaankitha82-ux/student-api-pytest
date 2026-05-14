d3c1c2843cc1e9cfb59c143f930ba24a9ed5452e
Ankitha Mallela <mallelaankitha82@gmail.com>
2026-05-14 19:15:47 +0530

    Fix test - expect 3 students instead of 2

--- tests/test_students.py
+++ tests/test_students.py
@@ -1,7 +1,7 @@
 def test_get_students(client):
     response = client.get("/students")
     assert response.status_code == 200
-    assert len(response.json()) == 2
+    assert len(response.json()) == 3  
 
 def test_read_root(client):
     response = client.get("/")
